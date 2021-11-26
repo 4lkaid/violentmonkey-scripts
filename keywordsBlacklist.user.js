251869f8570309700d0d42be1f2ee4dd85b6f867
4lkaid <4lkaid@gmail.com>
2021-11-26 17:24:33 +0800

    feat: 格式化关键词

--- keywordsBlacklist.user.js
+++ keywordsBlacklist.user.js
@@ -11,29 +11,22 @@
 // @name        关键词黑名单
 // @namespace   https://github.com/4lkaid/violentmonkey-scripts
 // @run-at      document-end
-// @version     1.1
+// @version     1.2
 // ==/UserScript==
+const tmp = GM_getValue('blacklist', '');
+tmp && tmp.replaceAll(' ', '') ?
+    GM_setValue('blacklist', tmp.replaceAll(' ', '')) :
+    GM_deleteValue('blacklist');
 GM_registerMenuCommand('点击设置词库黑名单', () => {
     const blacklist = prompt(
         '请输入要过滤的关键词, 用\'#\'分隔',
         GM_getValue('blacklist', '')
     );
-    if (blacklist) {
-        GM_setValue('blacklist', blacklist);
-    } else {
-        GM_deleteValue('blacklist');
-    }
+    GM_setValue('blacklist', blacklist);
     location.reload();
 });
-const blacklistArr = Array.from(
-    new Set(
-        Array.from(
-            GM_getValue('blacklist', 'csdn').split('#'),
-            (item) => item.trim()
-        ).filter((item) => Boolean(item))
-    )
-);
-const blacklistStr = blacklistArr.length > 0 ? ' -' + blacklistArr.join(' -') : '';
+const blacklistArr = GM_getValue('blacklist', 'csdn').split('#');
+const blacklistStr = ' -' + blacklistArr.join(' -');
 const regexp = new RegExp('(^| )-(' + blacklistArr.join('|') + ')(?!\\S)', 'g');
 document.getElementById('form').onsubmit = () => {
     const kw = document.getElementById('kw');
